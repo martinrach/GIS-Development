ae69d8e8dfd5a4d1facbd83008a2f798f029d536
Janne Niskanen <janne.niskanen@aalto.fi>
2023-03-23 16:57:49 +0200

    Adding folder

--- Visualize_data.py
+++
@@ -1,6 +0,0 @@
-import open3d as o3d
-
-def visualize_data(data):
-    geom = o3d.geometry.PointCloud()
-    geom.points = o3d.utility.Vector3dVector(data)
-    o3d.visualization.draw_geometries([geom])
