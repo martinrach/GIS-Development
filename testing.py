826c44431e5962f45e51439046b1b1542d783892
martinrach <122384923+martinrach@users.noreply.github.com>
2023-05-08 22:40:37 +0300

    Add files via upload

--- testing.py
+++ testing.py
@@ -32,7 +32,7 @@ if os.path.exists(output_file):
 
 ####### define parameters #######
 # antenna
-antenna_height = int(50)
+antenna_height = int(100)
 antenna_distance = int(50000)
 
 
@@ -67,67 +67,104 @@ print("Band successfully imported!")
 
 
 
-
-
-
-
-
 ###############################################################
 ###############################################################
 ####### start method #######
 ###############################################################
 ###############################################################
-# 1. iteration viewshed
-
 # turn band into array (possible antenna stations)
 possible_antenna = band.ReadAsArray()
 
-# find highest point (index)
-index = np.unravel_index(np.argmax(possible_antenna, axis=None), 
-                         possible_antenna.shape)
 
-# position of highest point
-obsX = int(xmin) + int(index[0])
-obsY = int(ymax) - int(index[1])
+####### stuff for iteration #######
+max_number_of_stations = 20
+antenna_list = []
+coverage = 0
+iteration_count = 0
+coverage_treshold = 98
+indices = []
 
 
-# calculate viewshed
-dataset = gdal.ViewshedGenerate(srcBand = band, 
-                                driverName='GTiff', 
-                                targetRasterName=output_file, 
-                                creationOptions=['COMPRESS=NONE'], 
-                                observerX=obsX, 
-                                observerY=obsY, 
-                                observerHeight=antenna_height, 
-                                targetHeight=0, 
-                                maxDistance=antenna_distance, 
-                                visibleVal=1, 
-                                invisibleVal=0, 
-                                outOfRangeVal=0, 
-                                noDataVal=2, 
-                                dfCurvCoeff=1 - 1/7, 
-                                mode=1)
+####### start iteration #######
+while coverage < coverage_treshold and len(antenna_list) < max_number_of_stations:
+    # find highest point (index)
+    index = np.unravel_index(np.argmax(possible_antenna, axis=None), 
+                             possible_antenna.shape)
+        
+    # check if new antenna is too close to existing antenna   
+    if np.isin(indices, [np.arange(index[0]-25, index[0]+25), np.arange(index[1]-25,index[1]+25)]).any():
+        # if too close, mark it as not possible antenna position
+        possible_antenna[index[0], index[1]] = 0
+        continue
 
-# turn viewshed into array and delete it
-new_viewshed = dataset.ReadAsArray()
-del dataset
+    # position of highest point
+    obsX = int(xmin) + int(index[0])
+    obsY = int(ymax) - int(index[1])
 
-# combine viewsheds
-viewshed = np.dstack([viewshed, new_viewshed]).max(axis=2)
+    # calculate viewshed
+    dataset = gdal.ViewshedGenerate(srcBand = band, 
+                                    driverName='GTiff', 
+                                    targetRasterName=output_file, 
+                                    creationOptions=['COMPRESS=NONE'], 
+                                    observerX=obsX, 
+                                    observerY=obsY, 
+                                    observerHeight=antenna_height, 
+                                    targetHeight=0, 
+                                    maxDistance=antenna_distance, 
+                                    visibleVal=1, 
+                                    invisibleVal=0, 
+                                    outOfRangeVal=0, 
+                                    noDataVal=2, 
+                                    dfCurvCoeff=1 - 1/7, 
+                                    mode=1)
 
-# mask (no antenna there)
-mask = 1 - viewshed
+    # turn viewshed into array and delete it
+    new_viewshed = dataset.ReadAsArray()
+    del dataset
+    
+    # check if the new viewshed contributes much to the result
+    if len(antenna_list) > 1:
+        changes = abs(np.count_nonzero(viewshed == 1) - np.count_nonzero(np.dstack([viewshed, new_viewshed]).max(axis=2) == 1))
+        if changes < 0.03*np.count_nonzero(viewshed == 0):
+            possible_antenna[index[0], index[1]] = 0
+            continue
 
-# no antenna at the same position
-possible_antenna[index[0], index[1]] = 0
+    # add the coordinates of the highest point to list (station locations)
+    antenna_list.append([obsX, obsY])
+    indices.append([index[0], index[1]])
 
-# get new possible antenna positions
-possible_antenna = ma.array(possible_antenna, mask=mask)
+    # combine viewsheds
+    viewshed = np.dstack([viewshed, new_viewshed]).max(axis=2)
+    
+    # calculate coverage
+    coverage = np.count_nonzero(viewshed) / viewshed.size * 100
+    print('{:.0f} % of the area is covered by the base stations.'.format(coverage))
 
-# plot results (viewshed, mask and possible antenna position)
-plt.matshow(viewshed)
-plt.matshow(mask)
-plt.matshow(possible_antenna)
+    
+    # get new possible antenna positions
+    possible_antenna = ma.array(possible_antenna, mask=viewshed)
+    #print(possible_antenna)
+
+    # plot results (viewshed, mask and possible antenna position)
+    plt.matshow(viewshed)
+    #plt.matshow(mask)
+    plt.matshow(possible_antenna)
+
+    iteration_count += 1
+
+    #print(iteration_count)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -157,44 +194,6 @@ plt.matshow(possible_antenna)
 
 
 
-
-# 2. iteration viewshed (the same again)
-index = np.unravel_index(np.argmax(possible_antenna, axis=None), possible_antenna.shape)
-obsX = int(xmin) + int(index[0])
-obsY = int(ymax) - int(index[1])
-
-
-dataset = gdal.ViewshedGenerate(srcBand = band, 
-                                driverName='GTiff', 
-                                targetRasterName=output_file, 
-                                creationOptions=['COMPRESS=NONE'], 
-                                observerX=obsX, 
-                                observerY=obsY, 
-                                observerHeight=200, 
-                                targetHeight=0, 
-                                maxDistance=50000, 
-                                visibleVal=1, 
-                                invisibleVal=0, 
-                                outOfRangeVal=0, 
-                                noDataVal=2, 
-                                dfCurvCoeff=1 - 1/7, 
-                                mode=1)
-
-new_viewshed = dataset.ReadAsArray()
-del dataset
-
-
-viewshed = np.dstack([viewshed, new_viewshed]).max(axis=2)
-mask = 1 - viewshed
-
-
-
-possible_antenna = ma.array(possible_antenna, mask=mask)
-
-
-plt.matshow(viewshed)
-plt.matshow(mask)
-plt.matshow(possible_antenna)
 
 
 print('code completed')
