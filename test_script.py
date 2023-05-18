e3ccfaa79506d5628345c3920f83c8340e929beb
hilla-aho <116489387+hilla-aho@users.noreply.github.com>
2023-05-18 13:34:55 +0300

    Add files via upload

--- test_script.py
+++ test_script.py
@@ -1,1 +1,19 @@
+
+import fiona
+from fiona.crs import CRS
+
+
+crs = CRS.from_epsg('3067')
+
+point = {'geometry': {
+    'type': 'Point',
+    'coordinates': (268280,6731924)},
+    'properties': dict([('name', 'test_point')])}
+
+schema = {'geometry': 'Point',
+          'properties': dict([('name', 'str')])}
+
+with fiona.open('vector_csc_output_file.gpkg', 'w', driver='GPKG', crs=crs, schema=schema) as output:
+    output.write(point)
+    
 print('Testing of this python script is complete.')
